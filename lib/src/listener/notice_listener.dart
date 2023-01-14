b3690a7d546423f8f6c688882ee2a2078b987600
Dizzy <739091458@qq.com>
2023-01-14 13:22:24 +0800

    Update ack notice

--- lib/src/listener/notice_listener.dart
+++ lib/src/listener/notice_listener.dart
@@ -3,14 +3,14 @@ import 'package:xxim_sdk_flutter/src/model/notice_model.dart';
 /// 通知监听
 class NoticeListener {
   final Future<bool> Function(
-    List<NoticeModel> noticeModelList,
+    NoticeModel noticeModel,
   ) onReceive; // 接收
 
   NoticeListener({
     required this.onReceive,
   });
 
-  Future<bool> receive(List<NoticeModel> noticeModelList) {
-    return onReceive(noticeModelList);
+  Future<bool> receive(NoticeModel noticeModel) {
+    return onReceive(noticeModel);
   }
 }
