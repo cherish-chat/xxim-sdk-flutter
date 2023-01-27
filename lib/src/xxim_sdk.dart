38aaf85976225cd30ff36398122f5064eae98959
Dizzy <739091458@qq.com>
2023-01-27 17:57:11 +0800

    Update close database

--- lib/src/xxim_sdk.dart
+++ lib/src/xxim_sdk.dart
@@ -44,12 +44,12 @@ class XXIMSDK {
         requestTimeout: requestTimeout,
         connectListener: ConnectListener(
           onConnecting: connectListener.onConnecting,
-          onSuccess: () {
-            connectListener.onSuccess();
-            Future.doWhile(() async {
+          onSuccess: () async {
+            await Future.doWhile(() async {
               await Future.delayed(const Duration(milliseconds: 5));
               return !(await setCxnParams(cxnParams));
             });
+            connectListener.onSuccess();
           },
           onClose: connectListener.onClose,
         ),
@@ -134,7 +134,6 @@ class XXIMSDK {
       ),
     );
     if (resp == null) return false;
-    await _sdkManager?.closeDatabase();
     await _sdkManager?.openDatabase(
       userId: userId,
       isarName: isarName,
